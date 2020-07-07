5c50ef80ee19ad92cd2b4223c37ae17d53a0d1f3
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-07 08:49:55 -0600

    user update resolver now searches by logged in user id

--- code/api/src/modules/user/resolvers.js
+++ code/api/src/modules/user/resolvers.js
@@ -8,14 +8,10 @@ import params from '../../config/params'
 import models from '../../setup/models'
 
 // Update
-export async function update(parentValue, { id, style }, { auth }) {
-  if(auth.user.id == id) {
-    const user = await models.User.findOne({where: { id } })
-    if (user){
-      return await user.update({ style })
-    } else {
-      throw new Error(`User id:${id} does not exist`)
-    }
+export async function update(parentValue, { style }, { auth }) {
+  if(auth.user) {
+    const user = await models.User.findByPk(auth.user.id)
+    return await user.update({ style })
   } else {
     throw new Error('Operation denied.')
   }
