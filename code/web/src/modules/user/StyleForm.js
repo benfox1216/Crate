d651c7a46a6ab0b41f48bc71a61a49850e6a3816
Ally Jarjour <allyjarjour@gmail.com>
2020-07-09 13:09:05 -0600

    Add box shadow on images, update font of subheading and add {setStyle} to connect

--- code/web/src/modules/user/StyleForm.js
+++ code/web/src/modules/user/StyleForm.js
@@ -10,6 +10,8 @@ import { Grid, GridCell } from "../../ui/grid";
 import { H3, H4 } from "../../ui/typography";
 import Button from "../../ui/button";
 import { grey, grey2 } from "../../ui/common/colors";
+import { level1 } from "../../ui/common/shadows";
+
 
 // App Imports
 import userRoutes from "../../setup/routes/user";
@@ -40,42 +42,66 @@ class StyleForm extends Component {
 		const summaryArr = [...new Set(styleArr)];
 		summaryArr.forEach((style) => (summary += `${style} and `));
 		const style = summary.substr(0, summary.length - 4);
-		setStyle(style);
+		this.props.setStyle(style);
 		return style;
 	};
 
 	render() {
 		let images = (
-			<>
-				<img
-					onClick={(e) => this.addStyle(e)}
-					id="victorian"
-					src={`/images/style-form/goth_${
-						this.state.categories[this.state.counter]
-					}.jpg`}
-					style={{ width: "30%", cursor: "pointer", margin: ".5em" }}
-				/>
-				<img
-					onClick={(e) => this.addStyle(e)}
-					id="goth"
-					src={`/images/style-form/victorian_${
-						this.state.categories[this.state.counter]
-					}.jpg`}
-					style={{ width: "30%", cursor: "pointer", margin: ".5em" }}
-				/>
-				<img
-					onClick={(e) => this.addStyle(e)}
-					id="cosplay"
-					src={`/images/style-form/cosplay_${
-						this.state.categories[this.state.counter]
-					}.jpg`}
-					style={{ width: "30%", cursor: "pointer", margin: ".5em" }}
-				/>
-			</>
-		);
+      <>
+        <img
+          onClick={(e) => this.addStyle(e)}
+          id="goth"
+          src={`/images/style-form/goth_${
+            this.state.categories[this.state.counter]
+          }.jpg`}
+          style={{
+            width: "30%",
+            cursor: "pointer",
+            margin: ".5em",
+            boxShadow: level1,
+          }}
+          alt={`Photo of goth-style ${
+            this.state.categories[this.state.counter]
+          }`}
+        />
+        <img
+          onClick={(e) => this.addStyle(e)}
+          id="victorian"
+          src={`/images/style-form/victorian_${
+            this.state.categories[this.state.counter]
+          }.jpg`}
+          style={{
+            width: "30%",
+            cursor: "pointer",
+            margin: ".5em",
+            boxShadow: level1,
+          }}
+          alt={`Photo of victorian-style ${
+            this.state.categories[this.state.counter]
+          }`}
+        />
+        <img
+          onClick={(e) => this.addStyle(e)}
+          id="cosplay"
+          src={`/images/style-form/cosplay_${
+            this.state.categories[this.state.counter]
+          }.jpg`}
+          style={{
+            width: "30%",
+            cursor: "pointer",
+            margin: ".5em",
+            boxShadow: level1,
+          }}
+          alt={`Photo of cosplay-style ${
+            this.state.categories[this.state.counter]
+          }`}
+        />
+      </>
+    );
 
 		const subscriptionBtn = (
-			<Link to={userRoutes.subscriptions.path} style={{ margin: "46.5%" }}>
+			<Link to={userRoutes.subscriptions.path} style={{ margin: "45%" }}>
 				<Button theme="primary">My subscriptions</Button>
 			</Link>
 		);
@@ -83,31 +109,32 @@ class StyleForm extends Component {
 		const surveyOver = this.state.counter === 4;
 
 		return (
-			<div>
-				{/* SEO */}
-				<Helmet>
-					<title>Style Preferences</title>
-				</Helmet>
+      <div>
+        {/* SEO */}
+        <Helmet>
+          <title>Style Preferences</title>
+        </Helmet>
 
-				{/* Top title bar */}
-				<Grid style={{ backgroundColor: grey }}>
-					<GridCell style={{ padding: "2em", textAlign: "center" }}>
-						<H3 font="secondary">Style TEST</H3>
-						<H4 font="secondary">
-							{(surveyOver && this.summarizeStyle(this.state.styles)) ||
-								`Pick a style for ${this.state.categories[this.state.counter]}`}
-						</H4>
-					</GridCell>
-				</Grid>
+        {/* Top title bar */}
+        <Grid style={{ backgroundColor: grey }}>
+          <GridCell style={{ padding: "2em", textAlign: "center" }}>
+            <H3 font="secondary">Style TEST</H3>
+            <p style={{margin: "1em", color: "rgb(153, 153, 153)", fontSize: '25px'}}>
+              {(surveyOver && this.summarizeStyle(this.state.styles)) ||
+                `Pick the set of ${this.state.categories[this.state.counter]}
+								that makes your heart sing`}
+            </p>
+          </GridCell>
+        </Grid>
 
-				<Grid>
-					<GridCell style={{ padding: "1em", textAlign: "center" }}>
-						{surveyOver || images}
-					</GridCell>
-				</Grid>
-				{surveyOver && subscriptionBtn}
-			</div>
-		);
+        <Grid>
+          <GridCell style={{ padding: "1em", textAlign: "center" }}>
+            {surveyOver || images}
+          </GridCell>
+        </Grid>
+        {surveyOver && subscriptionBtn}
+      </div>
+    );
 	}
 }
 
@@ -124,4 +151,4 @@ function profileState(state) {
 	};
 }
 
-export default connect(profileState)(StyleForm);
+export default connect(profileState, { setStyle })(StyleForm);
