3d26fd8cfa001ba9837ae2e597789d8216c54cbe
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-09 15:14:33 -0600

    WIP user testing

--- code/api/__tests__/modules/user.test.js
+++ code/api/__tests__/modules/user.test.js
@@ -4,9 +4,9 @@ import graphqlHTTP from 'express-graphql';
 import schema from '../../src/setup/schema'
 
 describe("User mutations", () => {
-  let server;
-  beforeAll(()=> {
-    server = express();
+  let server = express();
+  let token = ""
+  beforeAll(async () => {
     server.use(
       '/',
       graphqlHTTP({
@@ -14,18 +14,31 @@ describe("User mutations", () => {
         graphiql: false
       })
     );
+
     const response = await request(server)
-    .post('/')
-    .send({ query: 'query { userLogin(style: \"Elliot is a brony\") { name id style } }'})
+      .get('/')
+      .send({ query: 'query { userLogin(email: \"user@crate.com\", password: \"123456\") { token } }' })
+    token = response.body.data.userLogin.token
   });
+
+  it('Gets all users', async () => {
+    const response = await request(server)
+      .get('/')
+      .send({ query: 'query { users{ name } }' })
+      .expect(200)
+    expect(response.body.data.users[0]).toEqual({ name: "The Admin" })
+    expect(response.body.data.users[1]).toEqual({ name: "The User" })
+  });
+
   it('Updates a user style', async () => {
-     const response = await request(server)
-     .post('/')
-     .send({ query: 'mutation { styleUpdate(style: \"Elliot is a brony\") { name id style } }'})
-     .expect(200)
-     console.log(response.body.data)
-     expect(response.body.data.styleUpdate.name).toEqual('The User')
-     expect(response.body.data.styleUpdate.id).toEqual(2)
-     expect(response.body.data.styleUpdate.style).toEqual('Elliot is a brony')
-   })
+    const response = await request(server)
+      .post('/')
+      .set('Accept', 'application/json')
+      .send({ query: 'mutation { styleUpdate(style: \"Vintage\") { name } }' })
+      .expect(200)
+    console.log(response.body)
+    // expect(response.body.data.styleUpdate.name).toEqual('The User');
+    // expect(response.body.data.styleUpdate.id).toEqual(2);
+    // expect(response.body.data.styleUpdate.style).toEqual('Elliot is a brony');
+  });
 })
