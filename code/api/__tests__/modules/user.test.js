35f7e0e0b7c0f8cd9d55a759b96e93cdc7430765
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-12 14:28:50 -0600

    Add authentication to test server

--- code/api/__tests__/modules/user.test.js
+++ code/api/__tests__/modules/user.test.js
@@ -3,49 +3,44 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from '../../src/setup/schema';
 import models from '../../src/setup/models';
+import authentication from '../../src/setup/authentication'
+
+
 
 describe("User queries", () => {
   let server = express();
   let token = ""
   beforeAll(async () => {
-    server.use(
-      '/',
-      graphqlHTTP({
-        schema: schema,
-        graphiql: false
-      })
-    );
+    server.use(authentication)
+    // API (GraphQL on route `/`)
+    server.use('/', graphqlHTTP(request => ({
+      schema,
+      graphiql: false,
+      context: {
+        auth: {
+          user: request.user,
+          isAuthenticated: request.user && request.user.id > 0
+        }
+      }
+    })));
 
-    const response = await request(server)
+    const loginResponse = await request(server)
       .get('/')
       .send({ query: 'query { userLogin(email: \"user@crate.com\", password: \"123456\") { token } }' })
       .expect(200)
 
-    token = response.body.data.userLogin.token
+    token = 'Bearer ' + loginResponse.body.data.userLogin.token
   });
 
-  // it('Updates a user style', async () => {
-  //   let user = await models.User.findOne({ where: { email: "user@crate.com" } })
-  //   const userDetails = user.get()
-  //   // console.log(userDetails)
-  //
-  //
-  //   const response = await request(server)
-  //     .post('/')
-  //     .set('Accept', 'application/json')
-  //     .send({ query: `mutation { styleUpdate(style: \"Vintage\", auth: { user: ${userDetails} }) { name } }` })
-  //     // .expect(200)
-  //   console.log(response.body.data)
-  //
-  //   // expect(response.body.data.styleUpdate.name).toEqual('The User');
-  //   // expect(response.body.data.styleUpdate.id).toEqual(2);
-  // });
+  it('Updates a user style', async () => {
+
+  });
 
   it('Gets all users', async () => {
     const response = await request(server)
-    .get('/')
-    .send({ query: 'query { users{ name } }' })
-    .expect(200)
+      .get('/')
+      .send({ query: 'query { users{ name } }' })
+      .expect(200)
 
     expect(response.body.data.users[0]).toEqual({ name: "The Admin" })
     expect(response.body.data.users[1]).toEqual({ name: "The User" })
@@ -53,9 +48,9 @@ describe("User queries", () => {
 
   it('Gets user by ID', async () => {
     const response = await request(server)
-    .get('/')
-    .send({ query: 'query { user(id: 1) { name } }' })
-    .expect(200)
+      .get('/')
+      .send({ query: 'query { user(id: 1) { name } }' })
+      .expect(200)
 
     expect(response.body.data.user).toEqual({ name: "The Admin" })
   });
